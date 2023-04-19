9e1fe146cb3b34e163e71fcbbb44d8cae96f495e
SolstarterVarrus <varrus@solstarter.org>
2023-04-19 15:21:49 -0400

    update: add all missing files

---
+++ lua/user/treesitter.lua
@@ -0,0 +1,29 @@
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+treesitter.setup({
+	highlight = {
+		enable = true,
+	},
+	indent = { enable = true },
+	autotag = { enable = true },
+	ensure_installed = {
+		"json",
+		"javascript",
+		"typescript",
+		"tsx",
+		"yaml",
+		"html",
+		"css",
+		"bash",
+		"lua",
+		"vim",
+		"dockerfile",
+		"gitignore",
+		"markdown",
+		"python",
+	},
+	auto_install = true,
+})
